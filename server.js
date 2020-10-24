e4576bf827a1334d9fc1685ac1ed9cbae1fe20f7
Thomas Fisher <thomasjfisher1@gmail.com>
2020-10-24 12:38:37 +1100

    added routes

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define routes
+// requiring path below makes /api/users pertain to just /
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
